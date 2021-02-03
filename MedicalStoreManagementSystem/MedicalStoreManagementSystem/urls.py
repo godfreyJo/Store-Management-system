d7d034c79a8d20acaa9ebd1eb668da1f384084b0
godfreyJo <godfreyoyugi@gmail.com>
2021-02-03 20:10:45 +0300

    importing routers

--- MedicalStoreManagementSystem/MedicalStoreManagementSystem/urls.py
+++ MedicalStoreManagementSystem/MedicalStoreManagementSystem/urls.py
@@ -11,6 +11,13 @@ router =routers.DefaultRouter()
 router.register("company", views.CompanyViewSet, basename='company')
 router.register("companybank", views.CompanyBankViewset, basename='companybank')
 router.register("medicine", views.MedicineViewSet, basename='medicine')
+router.register("companyaccount",views.CompanyAccountViewset,basename="companyaccount")
+router.register("employee",views.EmployeeViewset,basename="employee")
+router.register("employee_all_bank",views.EmployeeBankViewset,basename="employee_all_bank")
+router.register("employee_all_salary",views.EmployeeSalaryViewset,basename="employee_all_salary")
+router.register("generate_bill_api",views.GenerateBillViewSet,basename="generate_bill_api")
+router.register("customer_request",views.CustomerRequestViewset,basename="customer_request")
+router.register("home_api",views.HomeApiViewset,basename="home_api")
 
 import os
 
