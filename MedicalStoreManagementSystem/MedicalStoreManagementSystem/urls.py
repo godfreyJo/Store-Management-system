87353d6fd23836f046c0de0fcf5cf0c39983a8f9
godfreyJo <godfreyoyugi@gmail.com>
2021-02-03 20:07:46 +0300

    importing routes

--- MedicalStoreManagementSystem/MedicalStoreManagementSystem/urls.py
+++ MedicalStoreManagementSystem/MedicalStoreManagementSystem/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework import routers
 from MedicalApp import views
+from MedicalStoreManagementSystem import settings
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from MedicalApp.views import CompanyNameViewSet
+from MedicalApp.views import CompanyNameViewSet, MedicineByNameViewSet, CompanyOnlyViewSet
 
 router =routers.DefaultRouter()
 router.register("company", views.CompanyViewSet, basename='company')
 router.register("companybank", views.CompanyBankViewset, basename='companybank')
 router.register("medicine", views.MedicineViewSet, basename='medicine')
 
+import os
 
 
 urlpatterns = [
@@ -17,6 +20,9 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/gettoken/', TokenObtainPairView.as_view(), name='gettoken'),
     path('api/refresh_token/', TokenRefreshView.as_view(), name='refresh_token'),
-    path('api/companybyname/<str:name>', CompanyNameViewSet.as_view(), name='companybyname')
+    path('api/companybyname/<str:name>', CompanyNameViewSet.as_view(), name='companybyname'),
+    path('api/medicinebyname/<str:name>',MedicineByNameViewSet.as_view(),name="medicinebyname"),
+    path('api/companyonly/',CompanyOnlyViewSet.as_view(),name="companyonly"),
+    path('api/employee_bankby_id/<str:employee_id>',views.EmployeeBankByEIDViewSet.as_view(),name="employee_bankby_id"),
 
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
