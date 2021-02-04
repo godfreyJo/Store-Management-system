99e0edf26f871d60f9beb8bab6e4f6c2a18ac34c
godfreyJo <godfreyoyugi@gmail.com>
2021-02-04 22:21:54 +0300

    adding the react components

---
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './pages/Login'
+
+ReactDom.render(
+<Router>
+    <Switch>
+        <Route exact path="/" component={Login}>
+            
+        </Route>
+    </Switch>
+</Router>, document.getElementById("root"))
