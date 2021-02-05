5286271c277aef1928f429b668d26ecce4939b37
godfreyJo <godfreyoyugi@gmail.com>
2021-02-05 11:44:05 +0300

    authentication logic

--- MedicalStoreManagementSystem/msfrontend/frontend/src/pages/Login.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/pages/Login.js
@@ -11,31 +11,70 @@ class Login extends React.Component{
         username:"",
         password:"",
         btnDisabled: true,
+        loginStatus:0,
         
     };
     saveInputs=(event)=>{
         var key =event.target.name;
         this.setState({[key]:event.target.value});
         // eslint-disable-next-line eqeqeq
-        if(this.state.username !="" && this.state.password !=""){
-            this.setState({btnDisabled: false});
-        }
-        else{
-            this.setState({btnDisabled: true});
+        if(this.state.username != "" && this.state.password != ""){
+            this.setState({ btnDisabled: false });
+        } else {
+            this.setState({ btnDisabled: true });
 
         }
 
     };
-    formSubmit=(event)=>{
+    formSubmit = (event) => {
         event.preventDefault();
         console.log(this.state);
-        AuthHandler.login(this.state.username, this.state.password);
+        this.setState({loginStatus:1});
+        AuthHandler.login(this.state.username, this.state.password, this.handleAjaxResponse);
 
     };
-    handleAjaxResponse=(data)=>{
+    handleAjaxResponse = (data) => {
         console.log(data);
+        if(data.error) {
+            this.setState({ loginStatus: 4});
+        } else{
+            this.setState({loginStatus: 3});
+        }
+    };
+    getMessages = () =>{
+        if(this.state.loginStatus === 0){
+            return "";
+        } 
+        else if (this.state.loginStatus === 1){
+            return (
+                <div class="alert alert-warning">
+                    <strong>
+                        Logging in
+                    </strong> Please wait...
+                </div>
+            );
+        }
+         else if (this.state.loginStatus === 3){
+            return (
+                <div class="alert alert-success">
+                    <strong>
+                        Logging Successfull!
+                    </strong> 
+                </div>
+            );
+        }
+        else if (this.state.loginStatus === 4){
+            return (
+                <div class="alert alert-danger">
+                    <strong>
+                        Invalid Login Details
+                    </strong> 
+                </div>
+            );
+        }
 
     };
+
     render(){
         document.body.className='login-page';
         return(
@@ -60,7 +99,7 @@ class Login extends React.Component{
             />
             <div className="login-box">
             <div className="logo">
-                <a href="javascript:void(0);">Store Management</a>
+                <a href="index.js">Store Management</a>
                 
             </div>
             <div className="card">
@@ -101,6 +140,7 @@ class Login extends React.Component{
                                 <a href="forgot-password.html">Forgot Password?</a>
                             </div>
                         </div>
+                        {this.getMessages()}
                     </form>
                 </div>
             </div>
