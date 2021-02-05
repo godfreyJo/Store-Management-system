4b1ed9e0cbccd1d0ad4ecf9470bdad1b7aef254a
godfreyJo <godfreyoyugi@gmail.com>
2021-02-05 11:44:26 +0300

    refactoring authhandler

--- MedicalStoreManagementSystem/msfrontend/frontend/src/utils/AuthHandler.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/utils/AuthHandler.js
@@ -1,22 +1,26 @@
 import axios from "axios";
 import Config from "./Config";
-import {reactLocalStorage} from 'reactjs-localstorage'
+import {reactLocalStorage} from 'reactjs-localstorage';
 
 
 
 class AuthHandler{
-    static login(username, password,callback){
-        axios.post(Config.loginUrl,{username:username, password:password}).then(function(response){            
-                if (response.status===200) {
-                    reactLocalStorage.set("token",response.data.access)
-                    reactLocalStorage.set("refresh",response.data.refresh)                    
-                }
-            
+    static login(username, password, callback){
+        axios
+        .post(Config.loginUrl,{ username:username, password:password })
+        .then(function(response){            
+            if (response.status === 200) {
+                reactLocalStorage.set("token", response.data.access);
+                reactLocalStorage.set("refresh", response.data.refresh);    
+                callback({ error: false, message: "Login Successfull..." });               
+            }            
         })
-        .catch(function(error){
-            console.log(error.response);
-        })
-
+        .catch(function (error) {
+            callback({
+              error: true,
+              message: "Error During Login Invalid Login Details..",
+            });
+          });
     }
 }
 export default AuthHandler;
