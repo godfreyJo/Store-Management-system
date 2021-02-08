b19e5380cd21ffff73f8d4521000ddb506028ee1
godfreyJo <godfreyoyugi@gmail.com>
2021-02-08 08:23:27 +0300

    the Navbar addition

---
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/components/Navbar.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from "react";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a href="#" className="navbar-toggle" onClick={this.props.onBarClick}></a>
+            <a className="navbar-brand" href="index.html">
+              Medical Store Management System
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
