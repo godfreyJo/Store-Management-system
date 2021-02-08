233307144203b5d1c9adb46278c207ed15a62484
godfreyJo <godfreyoyugi@gmail.com>
2021-02-08 08:26:32 +0300

    definig the PrivateRoute

---
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/utils/PrivateRoute.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthHandler from './AuthHandler';
+
+
+
+export function PrivateRoute({ component: Component, ...rest }) {
+    return (
+        <Route
+            {...rest}
+            render={(props) => AuthHandler.loggedIn() ? < Component {...props} /> : <Redirect to='/' />} />
+    );
+}
