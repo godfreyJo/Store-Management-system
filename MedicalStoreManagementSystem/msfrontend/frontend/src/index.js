99d7260d250c8f1408c6f77d9f30476ddca6ee5e
godfreyJo <godfreyoyugi@gmail.com>
2021-02-08 08:22:34 +0300

    finding the missing UI

--- MedicalStoreManagementSystem/msfrontend/frontend/src/index.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import MainComponent from './components/MainComponent';
+import { PrivateRoute } from './utils/PrivateRoute';
 
 ReactDOM.render(
 <Router>
     <Switch>
-        <Route exact path="/" component={Login}>
+        <Route exact path="/" component={Login}> </Route>
+        <PrivateRoute exact path="/home" component={ MainComponent }> </PrivateRoute>
             
-        </Route>
+       
     </Switch>
-</Router>, document.getElementById("root"))
+</Router>, document.getElementById("root"));
