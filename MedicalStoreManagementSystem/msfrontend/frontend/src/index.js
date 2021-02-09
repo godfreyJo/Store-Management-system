6095953734df8722e3e1f1bd1f84be6ba515115c
godfreyJo <godfreyoyugi@gmail.com>
2021-02-09 07:19:53 +0300

    refactoring the code

--- MedicalStoreManagementSystem/msfrontend/frontend/src/index.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import MainComponent from './components/MainComponent';
-import { PrivateRoute } from './utils/PrivateRoute';
+// import { PrivateRoute } from './utils/PrivateRoute';
 
 ReactDOM.render(
 <Router>
     <Switch>
-        <Route exact path="/" component={Login}> </Route>
-        <PrivateRoute exact path="/home" component={ MainComponent }> </PrivateRoute>
+        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/home" component={ MainComponent }></Route>
             
        
     </Switch>
-</Router>, document.getElementById("root"));
+</Router>
+, document.getElementById("root")
+);
