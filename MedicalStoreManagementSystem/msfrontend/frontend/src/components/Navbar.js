438a4d97efff091082568baefa7fb6b9bb1e5ac4
godfreyJo <godfreyoyugi@gmail.com>
2021-02-12 22:59:16 +0300

    editing the current Navbar

--- MedicalStoreManagementSystem/msfrontend/frontend/src/components/Navbar.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/components/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends React.Component {
         <div className="container-fluid">
           <div className="navbar-header">
             <a href="#" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse"aria-expanded="false"></a>
-            <a href="#" className="bars" style="display: none;"></a>
+            <a href="#" className="bars" style={{display: "none"}}></a>
             <a className="navbar-brand" href="index.html">
               Medical Store Management System
             </a>
