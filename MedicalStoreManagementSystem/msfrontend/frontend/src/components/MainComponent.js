f9b6b10277444cebc6dbfc64b980852f623241c0
godfreyJo <godfreyoyugi@gmail.com>
2021-02-12 22:58:53 +0300

    cleaning the Maincomponent module

--- MedicalStoreManagementSystem/msfrontend/frontend/src/components/MainComponent.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/components/MainComponent.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import Overlay from  './Overlay';
+import Navbar from  './Navbar';
+import PageLoader from  './PageLoader';
+import Sidebar from  './Sidebar';
+import HomeComponent from  './HomeComponent';
+import GoogleFontLoader from 'react-google-font-loader';
+import  "adminbsb-materialdesign/css/themes/all-themes.css";
 
 class MainComponent extends React.Component {
   render(){
-    return<h1>Home</h1>
+    document.getElementById("root").className="theme-red"
+    return<React.Fragment>
+      <GoogleFontLoader
+                    fonts={[
+                        {
+                            font: 'Roboto',
+                            weights:[400, 700],
+                        }                        
+                    ]}
+                    subsets={['latin','cyrillic-ext']}
+            />
+            <GoogleFontLoader
+                    fonts={[
+                        {
+                            font: 'Material+Icons',
+                            
+                        }                        
+                    ]}
+                   
+            />
+      <Overlay/>
+      <Navbar/>
+      <Sidebar/>
+      <PageLoader/>
+      <HomeComponent/>
+    </React.Fragment>;
   }
 }
 
