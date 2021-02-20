97520375342f59947eb156e4b7a6f54414ad2b2b
godfreyJo <godfreyoyugi@gmail.com>
2021-02-20 09:18:17 +0300

    Create Conpmany Component

---
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/pages/CompanyComponent.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class CompanyComponent extends React.Component {
+  render() {
+    return (
+      <section className="content">
+        <div className="container-fluid">
+            <div className="block-header">
+                <h2>MANAGE COMPANY</h2>
+            </div>         
+                        
+
+            
+        </div>
+    </section>
+    );
+  }
+}
+
+export default CompanyComponent;
