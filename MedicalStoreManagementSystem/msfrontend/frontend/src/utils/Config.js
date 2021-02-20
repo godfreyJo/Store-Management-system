e96ce7adacfef2abac568a9f7b47b4614324ba12
godfreyJo <godfreyoyugi@gmail.com>
2021-02-20 12:38:38 +0300

    adding the contecnts of the side bar

--- MedicalStoreManagementSystem/msfrontend/frontend/src/utils/Config.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/utils/Config.js
@@ -2,5 +2,11 @@ class Config{
     static loginUrl="http://localhost:8000/api/gettoken/";
     static homeUrl="/home";
 
+    static sidebarItem=[
+        {"index":"0","title":"Home","url":"/home","icons":"home"},
+        {"index":"1","title":"Company","url":"/company","icons":"assessment"},
+        {"index":"0","title":"Home","url":"/home","icons":"home"},
+    ]
+
 }
 export default Config;
