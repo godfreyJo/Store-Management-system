ffce7a6dfbbd8ee98f9b1e84ea9ab9641baff16a
godfreyJo <godfreyoyugi@gmail.com>
2021-02-21 15:35:04 +0300

    deleting the unsed array

--- MedicalStoreManagementSystem/msfrontend/frontend/src/utils/Config.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/utils/Config.js
@@ -1,11 +1,12 @@
 class Config{
     static loginUrl="http://localhost:8000/api/gettoken/";
     static homeUrl="/home";
+    static logoutPageUrl = "/logout";
 
     static sidebarItem=[
         {"index":"0","title":"Home","url":"/home","icons":"home"},
         {"index":"1","title":"Company","url":"/company","icons":"assessment"},
-        {"index":"0","title":"Home","url":"/home","icons":"home"},
+       
     ]
 
 }
