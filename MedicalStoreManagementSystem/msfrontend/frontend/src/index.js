c57cf272a0f599e4deafbcd5047cb21639e9ae5e
godfreyJo <godfreyoyugi@gmail.com>
2021-02-21 15:37:46 +0300

    refactoring the index page

--- MedicalStoreManagementSystem/msfrontend/frontend/src/index.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/index.js
@@ -3,19 +3,30 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import MainComponent from './components/MainComponent';
+import HomeComponent from './pages/HomeComponent';
+import LogoutComponent from './pages/LogoutComponent';
 import CompanyComponent from './pages/CompanyComponent';
 // import { PrivateRoute } from './utils/PrivateRoute';
 import { PrivateRouteNew } from "./utils/PrivateRouteNew";
+import Config from "./utils/Config";
 
 ReactDOM.render(
 <Router>
     <Switch>
         <Route exact path="/" component={Login}></Route>
-        <PrivateRouteNew exact path="/home" activepage="0" page={ <MainComponent />}></PrivateRouteNew>
-        <PrivateRouteNew exact path="/company" activepage="1" page={ <CompanyComponent />}></PrivateRouteNew>
-            
-       
+        <Route exact path={Config.logoutPageUrl} component={LogoutComponent}></Route>
+
+        <PrivateRouteNew 
+            exact 
+            path="/home" 
+            activepage="0" page={ <HomeComponent />}
+        ></PrivateRouteNew>
+        <PrivateRouteNew 
+            exact 
+            path="/company" 
+            activepage="1" page={ <CompanyComponent />}
+        ></PrivateRouteNew>
     </Switch>
-</Router>
-, document.getElementById("root")
+</Router>,
+ document.getElementById("root")
 );
