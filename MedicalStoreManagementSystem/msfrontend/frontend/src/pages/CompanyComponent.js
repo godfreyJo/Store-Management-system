7d7fe32fc6d9adeec493d64c5cb4c491764296d0
godfreyJo <godfreyoyugi@gmail.com>
2021-02-22 23:40:43 +0300

    adding the various fields

--- MedicalStoreManagementSystem/msfrontend/frontend/src/pages/CompanyComponent.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/pages/CompanyComponent.js
@@ -21,10 +21,76 @@ class CompanyComponent extends React.Component {
                                 <label htmlFor="email_address">Name</label>
                                 <div className="form-group">
                                     <div className="form-line">
-                                        <input type="text" id="email_address" className="form-control" placeholder="Enter your email address"/>
+                                        <input 
+                                            type="text" 
+                                            id="name" 
+                                            name = "name"
+                                            className="form-control" 
+                                            placeholder="Enter Company Name"/>
                                     </div>
                                 </div>                              
                                 <br />
+                                <label htmlFor="email_address">License No.</label>
+                                <div className="form-group">
+                                    <div className="form-line">
+                                        <input 
+                                            type="text" 
+                                            id="license_no" 
+                                            name="license_no"
+                                            className="form-control" 
+                                            placeholder="Enter License Number"/>
+                                    </div>
+                                </div>  
+                                <br />  
+                                <label htmlFor="email_address">Address</label>
+                                <div className="form-group">
+                                    <div className="form-line">
+                                        <input 
+                                            type="text" 
+                                            id="address" 
+                                            name = "address"
+                                            className="form-control" 
+                                            placeholder="Enter Company Address"/>
+                                    </div>
+                                </div>
+                                <br />    
+                                <label htmlFor="email_address">Contact Number</label>
+                                <div className="form-group">
+                                    <div className="form-line">
+                                        <input 
+                                            type="text" 
+                                            id="contact_no" 
+                                            name = "contact_no"
+                                            className="form-control" 
+                                            placeholder="Enter Company Contact Number"/>
+                                    </div>
+                                </div> 
+                                <br />                             
+                                <label htmlFor="email_address">Email Address</label>
+                                <div className="form-group">
+                                    <div className="form-line">
+                                        <input 
+                                            type="text" 
+                                            id="company_email"
+                                            name = "company_email"
+                                            className="form-control" 
+                                            placeholder="Enter Company Email Address"/>
+                                    </div>
+                                </div>
+                                <br />
+                                <label htmlFor="email_address">Description</label>
+                                <div className="form-group">
+                                    <div className="form-line">
+                                        <input 
+                                            type="text" 
+                                            id="description"
+                                            name = "description"
+                                            className="form-control" 
+                                            placeholder="Enter Description"/>
+                                    </div>
+                                </div>
+                                <br />
+                                                                                                                  
                                 <button type="button" className="btn btn-primary m-t-15 waves-effect">Add Company</button>
                             </form>
                         </div>
