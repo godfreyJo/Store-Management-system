9fd98ecbf5518ac8d1d983c132926221fa062d86
godfreyJo <godfreyoyugi@gmail.com>
2021-02-23 10:43:38 +0300

    APiHandler adding the save company data

--- MedicalStoreManagementSystem/msfrontend/frontend/src/utils/APIHandler.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/utils/APIHandler.js
@@ -1,22 +1,43 @@
+import { reactLocalStorage } from "reactjs-localstorage";
+
 const {default: AuthHandler} = require('./AuthHandler');
 const {default: Axios} = require('axios');
 const {default: Config} = require('./Config');
 
 
 class APIHandler{
-    async checkLogin(){
-    if (AutHandler.checkTokenExpiry((){
-        var response=await Axios.post(config.refreshApiUrl, {refesh:AuthHandler.getRefreshToken(),
-        });
-        console.log(response);
+    async checkLogin() {
+        if (AuthHandler.checkTokenExpiry()) {
+            var response = await Axios.post(Config.refreshApiUrl, {
+                refesh: AuthHandler.getRefreshToken(),
+            });
+            reactLocalStorage.set('token', response.data.access);
     }
 }
-async saveCompanyData(name,license_no,address,contact_no,email,description){
-    checkLogin();
 
+async saveCompanyData(
+    name,
+    license_no,
+    address,
+    contact_no,
+    email,
+    description
+    ){
+
+    this.checkLogin();
+     var response=await Axios.post(
+         Config.companyApiUrl, {
+             name:name, 
+             license_no:license_no, 
+             address:address, 
+             contact_no:contact_no, 
+             email:email, 
+             description:description
+            },
+        { headers: {Authorization:"Bearer "+AuthHandler.getLoginToken() } }
+        );
+        return response;
 }
 
-
-
 }
-export default APIHandler
+export default APIHandler;
