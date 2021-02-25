13dafc364e5048da05ed95c0029f2c8becb12136
godfreyJo <godfreyoyugi@gmail.com>
2021-02-25 13:10:39 +0300

    adding company detail component

--- MedicalStoreManagementSystem/msfrontend/frontend/src/index.js
+++ MedicalStoreManagementSystem/msfrontend/frontend/src/index.js
@@ -6,6 +6,7 @@ import MainComponent from './components/MainComponent';
 import HomeComponent from './pages/HomeComponent';
 import LogoutComponent from './pages/LogoutComponent';
 import CompanyComponent from './pages/CompanyComponent';
+import CompanyDetailsComponent from './pages/CompanyDetailsComponent';
 import { PrivateRoute } from './utils/PrivateRoute';
 import { PrivateRouteNew } from "./utils/PrivateRouteNew";
 import Config from "./utils/Config";
@@ -26,6 +27,11 @@ ReactDOM.render(
             exact 
             path="/company" 
             activepage="1" page={ CompanyComponent }
+        ></PrivateRouteNew>
+        <PrivateRouteNew 
+            exact 
+            path="/companydetails/:id" 
+            activepage="1" page={ CompanyDetailsComponent }
         ></PrivateRouteNew>
     </Switch>
 </Router>,
